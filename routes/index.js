dbc249527d56270b67229a7ff3657785a7cba18d
Tariq Ameer <1tariqameer@gmail.com>
2024-03-11 19:18:49 +0000

    add /map route for testing Leaflet map

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/map', (req, res) => {
+  res.render('map', { title: 'Map' });
+});
+
 module.exports = router;
