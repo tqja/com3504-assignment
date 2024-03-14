bf611007586bdcab94b1e9f8cba936c58a8aee53
Abdur-Rahman Muhsin <arm070602@gmail.com>
2024-03-14 12:51:12 +0000

    Improved schema design

--- models/observations.js
+++ models/observations.js
@@ -2,10 +2,45 @@ let mongoose = require('mongoose');
 
 let Schema = mongoose.Schema;
 
+const locationSchema = new Schema({
+    latitude: {
+        type: Number,
+        required: true,
+        min: -90,
+        max: 90
+    },
+    longitude: {
+        type: Number,
+        required: true,
+        min: -180,
+        max: 180
+    }
+});
+
+const commentSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        immutable: true
+    },
+    comment: {
+        type: String,
+        required: true,
+        immutable: true
+    },
+    timestamp: {
+        type: Date, required: true, immutable: true, max: Date.now()
+    }
+});
+
 let ObservationSchema = new Schema(
     {
-        posterNickname: { type: String, required: true, immutable: true },
-        image: { type: Buffer, required: true, immutable: true },
+        posterNickname: {
+            type: String, required: true, immutable: true
+        },
+        image: {
+            type: Buffer, required: true, immutable: true
+        },
         name: {
             type: String, required: true, maxLength: 100
         },
@@ -22,11 +57,11 @@ let ObservationSchema = new Schema(
             type: String, required: true, immutable: true, maxLength: 2500
         },
         location: {
-            type: String,
+            type: locationSchema,
             required: true,
             immutable: true
         },
-        comments: [{ username: String, comment: String, timestamp: Date }],
+        comments: [commentSchema],
         height: { type: Number, immutable: true }, // Make into enums...
         spread: { type: Number, immutable: true }, // ...
         sunlight: {
@@ -43,10 +78,10 @@ let ObservationSchema = new Schema(
             required: true,
             immutable: true
         },
-        flowering: { type: Boolean, immutable: true },
-        fragrant: { type: Boolean, immutable: true },
-        fruiting: { type: Boolean, immutable: true },
-        native: { type: Boolean, immutable: true }
+        flowering: { type: Boolean, default: true, immutable: true },
+        fragrant: { type: Boolean, default: true, immutable: true },
+        fruiting: { type: Boolean, default: true, immutable: true },
+        native: { type: Boolean, default: true, immutable: true }
     }
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const model = require('../models/observations');
+
 /* GET home page. */
 router.get('/', function(req, res) {
+  console.log(
+      new model({
+        posterNickname: "a",
+        image: 1,
+        name: "a",
+        //status:
+        dateSeen: Date.now(),
+        description: "a",
+        location: {latitude: 1, longitude: 1},
+        comments: [{username: "a", comment: "a", timestamp: Date.now()}],
+        height: 1,
+        spread: 1,
+        sunlight: "NA",
+        soil_type: "NA",
+      })
+  )
   res.render('index', { title: 'Express' });
 });
 
