3f67cc1046e6a4c129caa8aa021c214c70530bd1
Tariq Ameer <1tariqameer@gmail.com>
2024-03-29 16:21:41 +0000

    change URI to reference the IPv4 loopback address directly
    In Node 17+, localhost points to an IPv6 address instead of the IPv4 address. This causes the database connection to fail. See the following issue for more detail: https://github.com/Automattic/mongoose/issues/12366#issuecomment-1365878674

--- databases/observations.js
+++ databases/observations.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://localhost:27017/floraExplorerDB';
+const mongoDB = "mongodb://127.0.0.1:27017/floraExplorerDB";
 let connection;
 
 mongoose.Promise = global.Promise;
