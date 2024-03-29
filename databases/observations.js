4bbbb4478f74260464c0e7440bb048386b2bc370
Tariq Ameer <1tariqameer@gmail.com>
2024-03-29 16:21:53 +0000

    prettier reformat

--- databases/observations.js
+++ databases/observations.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoDB = "mongodb://127.0.0.1:27017/floraExplorerDB";
 let connection;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mongoDB).then(result => {
+mongoose
+  .connect(mongoDB)
+  .then((result) => {
     connection = result.connection;
     console.log("Connection successful!");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log("Connection failed!", err);
-});
+  });
