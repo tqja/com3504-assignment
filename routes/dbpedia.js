5a28a95797397227e17b6782aa0866dc58d784a8
Tariq Ameer <1tariqameer@gmail.com>
2024-03-30 01:06:04 +0000

    change sparqlQuery route to return data in response instead of passing to dbpedia route

--- routes/dbpedia.js
+++ routes/dbpedia.js
@@ -3,21 +3,18 @@ const router = express.Router();
 const { dbpediaController } = require('../controllers/dbpedia');
 
 /** Makes a SPARQL query to DBPedia. Retrieves data if successful, or an empty array otherwise. */
-const sparqlQuery = (req, res, next) => {
+const sparqlQuery = (req, res) => {
   dbpediaController(req, res)
     .then(data => {
-      req.queryData = data;
-    }).then(() => {
-      next();
-    }
-    ).catch(err => {
+      res.json(data);
+    })
+    .catch(err => {
       console.error(err);
-      req.queryData = [];
-      next();
+      res.status(500).json({ error: "Failed to fetch data" })
     });
 }
 
 router.get('/', dbpedia);
-router.get('/sparqlQuery', sparqlQuery, dbpedia);
+router.get('/sparqlQuery', sparqlQuery);
 
 module.exports = router;
