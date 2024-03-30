78052f2f5835a63c3bee628c4622e15938301f80
Tariq Ameer <1tariqameer@gmail.com>
2024-03-30 01:04:43 +0000

    Remove dbpedia route
    This route was used to prototype the dbpedia functionality. It is now implemented into the main application, so can be removed now.

--- routes/dbpedia.js
+++ routes/dbpedia.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { dbpediaController } = require('../controllers/dbpedia');
 
-/** Renders the dbpedia route, and passes context to data if available from a query. */
-const dbpedia = (req, res) => {
-  const context = req.queryData;
-  res.render('dbpedia', {title: 'DBPedia search', data: context});
-}
-
 /** Makes a SPARQL query to DBPedia. Retrieves data if successful, or an empty array otherwise. */
 const sparqlQuery = (req, res, next) => {
   dbpediaController(req, res)
