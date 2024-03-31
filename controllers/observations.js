1fe456152dd3711f828dae1e3053b5db1356b735
Tariq Ameer <1tariqameer@gmail.com>
2024-04-01 00:46:25 +0100

    prettier reformat

--- controllers/observations.js
+++ controllers/observations.js
@@ -1,49 +1,54 @@
-const observationModel = require('../models/observations');
+const observationModel = require("../models/observations");
 
 exports.create = function (userData, filePath) {
-    const flowering = !!userData.flowering;
-    const fragrant = !!userData.fragrant;
-    const fruiting = !!userData.fruiting;
-    const native = !!userData.native;
+  const flowering = !!userData.flowering;
+  const fragrant = !!userData.fragrant;
+  const fruiting = !!userData.fruiting;
+  const native = !!userData.native;
 
-    let observation = new observationModel({
-        nickname: userData.nickname,
-        name: userData.name,
-        image: filePath,
-        dateSeen: userData.dateSeen,
-        description: userData.description,
-        location: {
-            latitude: userData.latitude,
-            longitude: userData.longitude
-        },
-        height: userData.height,
-        spread: userData.spread,
-        sunlight: userData.sunlight,
-        soil_type: userData.soil_type,
-        flowering: flowering,
-        fragrant: fragrant,
-        fruiting: fruiting,
-        native: native
-    });
-    return observation.save().then(observation => {
-        console.log(observation);
+  let observation = new observationModel({
+    nickname: userData.nickname,
+    name: userData.name,
+    image: filePath,
+    dateSeen: userData.dateSeen,
+    description: userData.description,
+    location: {
+      latitude: userData.latitude,
+      longitude: userData.longitude,
+    },
+    height: userData.height,
+    spread: userData.spread,
+    sunlight: userData.sunlight,
     soilType: userData.soilType,
+    flowering: flowering,
+    fragrant: fragrant,
+    fruiting: fruiting,
+    native: native,
+  });
+  return observation
+    .save()
+    .then((observation) => {
+      console.log(observation);
 
-        return JSON.stringify(observation);
-    }).catch(err => {
-        // Log the error if saving fails
-        console.log(err);
+      return JSON.stringify(observation);
+    })
+    .catch((err) => {
+      // Log the error if saving fails
+      console.log(err);
 
-        // Return null in case of an error
-        return null;
+      // Return null in case of an error
+      return null;
     });
 };
 
 exports.getAll = () => {
-    return observationModel.find().then(observations => {
-        return JSON.stringify(observations)
-    }).catch(err => {
-        console.log(err);
-        return null;
+  return observationModel
+    .find()
+    .then((observations) => {
+      return JSON.stringify(observations);
+    })
+    .catch((err) => {
+      console.log(err);
+      return null;
     });
 };
