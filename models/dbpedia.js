136f7e12a77c7446a4f22ed32f7babea6038a6cc
Tariq Ameer <1tariqameer@gmail.com>
2024-04-18 15:51:08 +0100

    add prefix directives to query

--- models/dbpedia.js
+++ models/dbpedia.js
@@ -3,7 +3,7 @@ const { SparqlEndpointFetcher } = require("fetch-sparql-endpoint");
 const fetcher = new SparqlEndpointFetcher();
 
 const fetchFromDbpedia = async (name) => {
-// lowercase string, then uppercase first character, as formatted in DBPedia
+  // lowercase string, then uppercase first character, as formatted in DBPedia
   if (!name) {
     return [];
   }
@@ -13,42 +13,48 @@ const fetchFromDbpedia = async (name) => {
   return new Promise(async (resolve, reject) => {
     const bindingsArray = [];
     const query = `
-    SELECT DISTINCT ?plant ?commonName ?scientificName ?description
-      WHERE {
-        { ?plant rdfs:label "${name}"@en }
-        UNION
-        { ?plant dbo:binomial [ rdfs:label "${name}"@en ] }
-        
-        ?plant rdf:type ?type .
-        FILTER 
-          (?type IN (dbo:Eukaryote, dbo:Plant, dbo:FloweringPlant, dbo:Gnetophytes, dbo:Conifer, dbo:GreenAlga,
-          dbo:ClubMoss, dbo:Moss, dbo:Cycad, dbo:Fern, dbo:CultivatedVariety, dbo:Ginkgo)
-        )
-        
-        ?plant rdfs:label ?commonName ;
-               dbo:abstract ?description .
-        
-        OPTIONAL {
-          ?plant dbo:binomial [ rdfs:label ?scientificName ] .
-          FILTER (lang(?scientificName) = 'en')
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      PREFIX dbo: <http://dbpedia.org/ontology/>
+      
+      SELECT DISTINCT ?plant ?commonName ?scientificName ?description
+        WHERE {
+          { ?plant rdfs:label "${name}"@en }
+          UNION
+          { ?plant dbo:binomial [ rdfs:label "${name}"@en ] }
+          
+          ?plant rdf:type ?type .
+          FILTER 
+            (?type IN (dbo:Eukaryote, dbo:Plant, dbo:FloweringPlant, dbo:Gnetophytes, dbo:Conifer, dbo:GreenAlga,
+            dbo:ClubMoss, dbo:Moss, dbo:Cycad, dbo:Fern, dbo:CultivatedVariety, dbo:Ginkgo)
+          )
+          
+          ?plant rdfs:label ?commonName ;
+                 dbo:abstract ?description .
+          
+          OPTIONAL {
+            ?plant dbo:binomial [ rdfs:label ?scientificName ] .
+            FILTER (lang(?scientificName) = 'en')
+          }
+  
+          FILTER (lang(?commonName) = 'en')
+          FILTER (lang(?description) = 'en')
         }
+        LIMIT 1`;
 
-        FILTER (lang(?commonName) = 'en')
-        FILTER (lang(?description) = 'en')
-      }
-      LIMIT 1`;
+    const stream = await fetcher.fetchBindings(
+      "https://dbpedia.org/sparql",
+      query,
+    );
 
-    const stream = await fetcher.fetchBindings('https://dbpedia.org/sparql', query);
-
-    stream.on('data', (bindings) => {
+    stream.on("data", (bindings) => {
       bindingsArray.push(bindings);
     });
 
-    stream.on('error', (err) => {
+    stream.on("error", (err) => {
       reject(err);
     });
 
-    stream.on('end', () => {
+    stream.on("end", () => {
       // if subject has no binomial name, fill it with common name
       if (bindingsArray[0] && !bindingsArray[0].scientificName) {
         bindingsArray[0].scientificName = bindingsArray[0].commonName;
@@ -56,6 +62,6 @@ const fetchFromDbpedia = async (name) => {
       resolve(bindingsArray);
     });
   });
-}
+};
 
 module.exports = { fetchFromDbpedia };
